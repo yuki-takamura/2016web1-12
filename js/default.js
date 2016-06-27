17628535af5b3dbeb8916298e1353a312392448c
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-27 18:17:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,16 @@
 
 document.getElementById('form').onsubmit = function(){
   window.alert(document.getElementById('form').word.value);
-  return false;
+//  return false;
 };
 
-Number(word);
+var word = 100;
+var query = window.location.search.substring( 1 );
+var parameters = query.split( '&' );
+if(parameters){
+  var element = parameters[0].split( '=' );
+  word = parseInt(element[1]);
+}
 
 for(var i = 1; i < word; i++)
 {
