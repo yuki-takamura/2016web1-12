79f6b984829f421844542052db6e26451e5da99d
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-27 17:25:13 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,3 +3,16 @@ document.getElementById('form').onsubmit = function(){
   window.alert(docment.getElementById('form').number.value);
   return false;
 };
+
+var maxNum = number;
+
+for(var i = 1; i < maxNum; i++)
+{
+  var li = document.createElement('li');
+  if((i%3 != 0) && (i%5 != 0))
+    {
+      li.textContent = i;
+      document.getElementById('num').appendChild(li);
+    }
+    
+}
