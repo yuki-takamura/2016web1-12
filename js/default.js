7629a9473b22fbb56528ed9ec2fea42a3b376536
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-27 17:50:50 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -9,17 +9,18 @@ var maxNum = word - 0;
 for(var i = 1; i < maxNum; i++)
 {
   var li = document.createElement('li');
-  if((i%3 != 0) && (i%5 != 0)){
-      li.textContent = i;
-  }
-  else if((i%3 == 0) && (i%5 == 0)){
+  
+  if((i%3 == 0) && (i%5 == 0)) {
     li.textContent = 'FizzBuzz';
   }
-  else if(i%3 == 0){
+  else if(i%3 == 0) {
     li.textContent = 'Fizz';
   }
-  else if(i%5 == 0){
+  else if(i%5 == 0) {
     li.textContent = 'Buzz';
+  }
+  else {
+    li.textContent = i;
   }
  document.getElementById('list').appendChild(li);
 }
