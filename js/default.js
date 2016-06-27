d699151842e2610420b32b0edec1c4394747c3de
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2016-06-27 17:47:01 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -15,12 +15,10 @@ for(var i = 1; i < maxNum; i++)
   else if((i%3 == 0) && (i%5 == 0)){
     li.textContent = 'FizzBuzz';
   }
-  else if(i%3==0)
-  {
+  else if(i%3 == 0){
     li.textContent = 'Fizz';
   }
-  else if(i%5==0)
-  {
+  else if(i%5 == 0){
     li.textContent = 'Buzz';
   }
  document.getElementById('list').appendChild(li);
